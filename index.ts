92af1a4b4d6e5b2264d6e0cf3ff0a6d356db5035
nzambello <nzambello@protonmail.com>
2022-07-13 00:14:55 +0200

    Update example

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { client, content } from 'plone-restapi-client'
+import { client, content } from "plone-restapi-client";
 
-client.init('http://localhost:8080/Plone')
+client.init("http://localhost:8080/Plone");
 
-client.auth('admin', 'admin').then(() => {
-  content.get('/my-page').then(console.log)
-})
+client.auth("admin", "admin").then(() => {
+  content.get("/").then(console.log);
+});
